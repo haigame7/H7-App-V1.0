8523f646b8ce5c647e8a3f6e2e82a91cb9c42794
naivehhr <1903283729@qq.com>
2016-03-03 11:38:23 +0800

    注释

--- haigame7/app/styles/MainScreeStyle.js
+++ haigame7/app/styles/MainScreeStyle.js
@@ -1,8 +1,11 @@
-/**
- * Created by wangchenlong on 15/11/29.
- */
 'use strict';
 
+/**
+ * 首屏样式
+ * @param  {[type]} 'react-native' [description]
+ * @return {[type]}                [description]
+ * @author aran.hu
+ */
 var React = require('react-native');
 
 var {
