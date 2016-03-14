a444ef45ab8db3b9f64bf9622956ab1f28f47f62
Drex <382642143@qq.com>
2016-03-14 18:05:53 +0800

    Icons

--- haigame7/app/styles/userstyle.js
+++ haigame7/app/styles/userstyle.js
@@ -1,10 +1,7 @@
 'use strict';
 
-
 import { StyleSheet, Platform } from 'react-native';
 var Util = require('../view/common/util');
-
-
 export default StyleSheet.create({
     loginbg: {
         flex: 1,
@@ -30,7 +27,7 @@ export default StyleSheet.create({
         marginTop: 20,
         marginLeft: 36,
         marginRight: 36,
-        backgroundColor: 'rgba(0, 0, 0,0.6)',
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
     },
     logininputfont: {
         color: '#FFFFFF',
@@ -91,7 +88,7 @@ export default StyleSheet.create({
         alignSelf: 'stretch'
     },
     buttonDisabled: {
-        backgroundColor: 'rgba(240, 12, 12,0.61)',
+        backgroundColor: 'rgba(240, 12, 12,0.6)',
         padding: 15,
         marginTop: 20,
         justifyContent: 'center',
@@ -108,7 +105,7 @@ export default StyleSheet.create({
         alignSelf: 'flex-end'
     },
     cetifyButtonDisabled: {
-        backgroundColor: 'rgba(252, 204, 95,0.61)',
+        backgroundColor: 'rgba(252, 204, 95,0.6)',
         padding: 15,
         width: Util.size.width / 4,
         borderRadius: 5,
@@ -132,11 +129,6 @@ export default StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'stretch',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
     },
     instructions: {
         textAlign: 'center',
@@ -171,7 +163,7 @@ export default StyleSheet.create({
     },
     inputText: {
         marginLeft: 40,
-        backgroundColor: 'rgba(45, 50, 45, 0.68)',
+        backgroundColor: 'rgba(45, 50, 45, 0.6)',
         borderColor: 'rgb(240, 12, 12)',
         borderWidth: 1,
         borderRadius: 5,
@@ -214,13 +206,9 @@ export default StyleSheet.create({
         marginTop: -10,
         marginRight: 40
     },
-    switchSetting:{
-      marginTop:5,
-      left:Util.size.width*1/2 ,
-    },
     centerbg: {
        flex:1,
-       backgroundColor:'rgb(70, 70, 70)',
+       backgroundColor:'rgb(0, 0, 0)',
        height: Util.size.height,
        width: Util.size.width,
    },
@@ -271,6 +259,7 @@ export default StyleSheet.create({
      marginRight:40,
      textAlign:'center',
      top:Util.size.height*1/20
+
    },
    centerlitext:{
      marginTop:10,
@@ -299,6 +288,7 @@ export default StyleSheet.create({
      marginTop:20,
      backgroundColor:'rgb(255,255,255)',
      width:Util.pixel,
+
      height:Util.size.width*1/20,
      marginBottom:20,
 
@@ -312,163 +302,7 @@ export default StyleSheet.create({
    centerlicontent:{
      flexDirection:'row',
    },
-   centertextarea:{
-     height:Util.size.height*1/3,
-     top:10,
-     marginLeft:20,
-     fontSize:15,
-     color:'rgb(120,120,120)',
-     width:Util.size.width-40,
-     backgroundColor:'rgb(50,50,50)',
-   },
-   centertextareacount:{
-    position:'absolute',
-    fontSize:15,
-    color:'rgb(120,120,120)',
-    right:30,
-    marginTop:-15,
-    backgroundColor:'rgb(50,50,50)',
-   },
-   infotextright:{
-     marginTop:25,
-     position:'absolute',
-     right: Util.size.width*1/8,
-     color:'rgb(90,90,90)',
-   },
-   infoangelright:{
-     marginTop:20,
-     position:'absolute',
-     left: Util.size.width*10/11,
-     color:'rgb(90,90,90)',
-   },
-   infoimage:{
-    width:78,
-    height:78,
-    borderRadius: 39,
-    left: Util.size.width*4/7,
-    top:Util.size.height*1/40-10
-   },
-   infosplit: {
-     position:'absolute',
-     left: 10,
-     marginTop:58,
-     backgroundColor:'rgb(50,50,50)',
-     width:Util.size.width*10/11,
-     height: Util.pixel,
-   },
-  button: {
-    backgroundColor: 'rgb(240, 12, 12)',
-    padding: 15,
-    marginTop: 20,
-    justifyContent: 'center',
-    alignSelf: 'stretch'
-  },
-  buttonDisabled: {
-    backgroundColor: 'rgba(240, 12, 12,0.61)',
-    padding: 15,
-    marginTop: 20,
-    justifyContent: 'center',
-    alignSelf: 'stretch'
-  },
-  certifyButton: {
-    backgroundColor: 'rgb(252, 204, 95)',
-    padding: 15,
-    width:Util.size.width/4,
-    height:35,
-    borderRadius:5,
-    marginTop: -40,
-    marginRight:5,
-    alignSelf: 'flex-end'
-  },
-  cetifyButtonDisabled: {
-    backgroundColor: 'rgba(252, 204, 95,0.61)',
-    padding: 15,
-    width:Util.size.width/4,
-    borderRadius:5,
-    marginTop: 20,
-    justifyContent: 'center',
-    alignSelf: 'stretch'
-  },
-
-  buttonText: {
-    fontSize: 15,
-    color: 'white',
-    alignSelf: 'center'
-  },
-  certifyButtonText:{
-    fontSize: 11,
-    color: 'black',
-    marginTop:-3,
-    alignSelf: 'center'
-
-  },
-  container: {
-    flex: 1,
-    alignItems: 'stretch',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  toolbar: {
-    height: 60,
-    backgroundColor: '#D6D2D2'
-  },
-  orText: {
-    alignSelf: 'center',
-    marginTop: 20
-  },
-  message: {
-    color: 'red',
-    marginLeft: 5
-  },
-  inputContainer: {
-    width:Util.size.width-40,
-    height:80,
-    marginTop:20,
-    borderBottomWidth: 1,
-    borderBottomColor: 'transparent'
-  },
-  inputContainerSecond: {
-    width:Util.size.width-40,
-    height:80,
-    marginTop:-10,
-    borderBottomWidth: 1,
-    borderBottomColor: 'transparent'
-  },
-  inputText: {
-    marginLeft:40,
-    backgroundColor: 'rgba(45, 50, 45, 0.68)',
-    borderColor:'rgb(240, 12, 12)',
-    borderWidth:1,
-    borderRadius:5,
-    height: 45
-  },
-  linkText:{
-   flex:1,
-   flexDirection:'row',
-   alignItems:'flex-start',
-   margin:8
-  },
-  linkTextLeft:{
-  fontSize:13,
-  color:'#fff',
-  marginTop:-20,
-  marginLeft:40,
-  },
-  linkTextRight:{
-   fontSize:13,
-   marginTop:-20,
-   color:'rgb(46, 97, 209)',
-   marginLeft:Util.size.width/2-40,
-  },
-   setpwdText:{
+  setpwdText:{
     marginLeft:40,
     width:Util.size.width-60,
     color:'rgb(121, 121, 121)',
