92aaabef285d5fd0ce1b85d8d57877df52e35913
naivehhr <1903283729@qq.com>
2016-03-14 15:36:53 +0800

    123

---
+++ haigame7/app/view/common/lefticon.js
@@ -0,0 +1,20 @@
+var React = require('react-native');
+
+var CommonStyle = require('../../styles/commonstyle');
+var {
+  StyleSheet,
+  Text,
+  View
+  } = React;
+
+module.exports = React.createClass({
+  render: function(){
+    return (
+      <View>
+        <View style={CommonStyle.headerGo}>
+        </View>
+      </View>
+    );
+  }
+
+});
