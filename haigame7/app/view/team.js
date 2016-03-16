baf88991c2966f1b65aa2bc6e859ebef1146ff38
Horace1117 <ghr1117@163.com>
2016-03-16 17:42:16 +0800

    2016年03月16日17:42:14

--- haigame7/app/view/team.js
+++ haigame7/app/view/team.js
@@ -7,7 +7,6 @@
  * @return {[Team Component]}
  * @author aran.hu
  */
-var React = require('react-native');
 var CommonStyle = require('../styles/commonstyle');
 var TeamStyle = require('../styles/teamstyle');
 var Loading = require('./common/loading');
@@ -15,57 +14,99 @@ var FindTeam = require('./team/findteam');
 var Util = require('./common/util');
 var Icon = require('react-native-vector-icons/FontAwesome');
 
-var {
+import React, {
   View,
   Text,
   Image,
   StyleSheet,
+  Component,
   TouchableOpacity,
   TouchableHighlight,
-  } = React;
+  } from 'react-native';
 
-var Team = React.createClass({
-
-  getInitialState() {
-    console.log('UserScreen Init Data');
-    return {
-      navbar: 0,
-      subnavbar:1,
-      invite:0,
-      data:{
-          subnavbarone:'我的申请',
-          subnavbartwo:'我的受邀',
-      },
-    };
-  },
-  componentDidMount:function()
+  export default class extends Component{
+    constructor(props) {
+      super(props);
+      this.state = {
+        navbar: 0,
+        subnavbar:1,
+        invite:0,
+        data:{
+            subnavbarone:'我的申请',
+            subnavbartwo:'我的受邀',
+        },
+      }
+    }
+  renderteamList(){
+    if(this.state.navbar==0){
+      return(
+        <View>
+        <View style={[styles.teamlist]}>
+        <Image style={[styles.teamimage,]} source={{uri:'http://images.haigame7.com/logo/20160216133928XXKqu4W0Z5j3PxEIK0zW6uUR3LY=.png'}} />
+         <View style={[styles.teamcontent]}>
+         <View style={[styles.teamrowtext]}>
+         <Text style={[styles.teamcontenttext,{fontSize:15,color:'rgb(230, 193, 39)',fontWeight:'bold'}]}>{'犀利拍立冬至'}</Text>
+          <Text style={[styles.teamcontenttext,{color:'red',margin:2}]}>{'2小时前'}</Text>
+         </View>
+         <Text style={[styles.teamcontenttext,]}>{'生命不息,电竞不止'}</Text>
+         <Text style={[styles.teamcontenttext,{fontSize:13,color:'rgb(200,200,200)',lineHeight:15}]}>{'本队需要辅助1名,擅长XX英雄,战队福利优厚，报名从速'}</Text>
+         </View>
+         <TouchableOpacity style = {this.state.invite==0 ? styles.teambuttoninvite : styles.teambuttoninvited}  >
+              <Text style = {this.state.invite==0 ? styles.btnfontinvite:styles.btnfontinvited}> { this.state.invite==0 ? '申请加入' : '已申请' } </Text>
+         </TouchableOpacity>
+        </View>
+         <View style={[styles.teamsplit]}></View>
+         <View style={[styles.teamlist]}>
+         <Image style={[styles.teamimage,]} source={{uri:'http://images.haigame7.com/logo/20160216133928XXKqu4W0Z5j3PxEIK0zW6uUR3LY=.png'}} />
+         <View style={[styles.teamcontent]}>
+         <View style={[styles.teamrowtext]}>
+         <Text style={[styles.teamcontenttext,{fontSize:15,color:'rgb(230, 193, 39)',fontWeight:'bold'}]}>{'犀利拍立冬至'}</Text>
+          <Text style={[styles.teamcontenttext,{color:'red',margin:2}]}>{'2小时前'}</Text>
+         </View>
+          <Text style={[styles.teamcontenttext,]}>{'生命不息,电竞不止'}</Text>
+         <Text style={[styles.teamcontenttext,{fontSize:13,color:'rgb(200,200,200)',lineHeight:15}]}>{'本队需要辅助1名,擅长XX英雄,战队福利优厚，报名从速'}</Text>
+         </View>
+         <TouchableOpacity style = {this.state.invite==0 ? styles.teambuttoninvite : styles.teambuttoninvited}  >
+              <Text style = {this.state.invite==0 ? styles.btnfontinvite:styles.btnfontinvited}> { this.state.invite==0 ? '申请加入' : '已申请' } </Text>
+         </TouchableOpacity>
+         </View>
+         <View style={[styles.teamsplit]}></View>
+         <View style={[styles.teamlist]}>
+         <Image style={[styles.teamimage,]} source={{uri:'http://images.haigame7.com/logo/20160216133928XXKqu4W0Z5j3PxEIK0zW6uUR3LY=.png'}} />
+         <View style={[styles.teamcontent]}>
+         <View style={[styles.teamrowtext]}>
+         <Text style={[styles.teamcontenttext,{fontSize:15,color:'rgb(230, 193, 39)',fontWeight:'bold'}]}>{'犀利拍立冬至'}</Text>
+          <Text style={[styles.teamcontenttext,{color:'red',margin:2}]}>{'2小时前'}</Text>
+         </View>
+         <Text style={[styles.teamcontenttext,]}>{'生命不息,电竞不止'}</Text>
+         <Text style={[styles.teamcontenttext,{fontSize:13,color:'rgb(200,200,200)',lineHeight:15}]}>{'本队需要辅助1名,擅长XX英雄,战队福利优厚，报名从速'}</Text>
+         </View>
+         <TouchableOpacity style = {this.state.invite==1 ? styles.teambuttoninvite : styles.teambuttoninvited}  >
+              <Text style = {this.state.invite==1 ? styles.btnfontinvite:styles.btnfontinvited}> { this.state.invite==1 ? '申请加入' : '已申请' } </Text>
+         </TouchableOpacity>
+         </View>
+          <View style={[styles.teamsplit]}></View>
+       </View>
+      );
+    }else{
+      return(
+       <View style={styles.item}>
+       </View>
+      );
+    }
+  }
+  render()
   {
-    this.setState({load:true});
-  },
-  _switchNavbar:function(nav){
-    var nameone ='我的申请';
-    var nametwo = '我的受邀';
-     if(nav==1){
-       nameone = '发出邀请';
-       nametwo = '申请加入';
-     }
-    this.setState({
-      navbar:nav,
-      data:{subnavbarone:nameone,subnavbartwo:nametwo},
-    });
-    return;
-  },
-  render: function ()
-  {
-    return(
+    let teamlist = this.renderteamList();
+    return (
       <View style={styles.container}>
 
         <View style={styles.nav}>
-          <TouchableOpacity style={styles.nav_item} onPress={this._switchNavbar.bind(this, 0)}>
+          <TouchableOpacity style={styles.nav_item}  onPress = {() => this._switchNavbar(0)}>
           <Text style={this.state.navbar==0?styles.nav_item_text_active:styles.nav_item_text}>加入战队</Text>
           <View style={this.state.navbar==0?styles.nav_item_line_active:styles.nav_item_line}></View>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.nav_item} onPress={this._switchNavbar.bind(this, 1)}>
+          <TouchableOpacity style={styles.nav_item}  onPress = {() => this._switchNavbar(1)}>
           <Text style={this.state.navbar==1?styles.nav_item_text_active:styles.nav_item_text}>招募队员</Text>
           <View style={this.state.navbar==1?styles.nav_item_line_active:styles.nav_item_line}></View>
           </TouchableOpacity>
@@ -83,47 +124,25 @@ var Team = React.createClass({
           <View style={[styles.infosplit]}></View>
           </View>
           <View style={[styles.centerbg]}>
-           <View style={[styles.teamlist]}>
-           <Image style={[styles.teamimage,]} source={{uri:'http://images.haigame7.com/logo/20160216133928XXKqu4W0Z5j3PxEIK0zW6uUR3LY=.png'}} />
-            <View style={[styles.teamcontent]}>
-            <Text style={[styles.teamcontenttext,{fontSize:15,color:'rgb(230, 193, 39)',fontWeight:'bold'}]}>{'犀利拍立冬至'}</Text>
-            <Text style={[styles.teamcontenttext,]}>{'生命不息,电竞不止'}</Text>
-            <Text style={[styles.teamcontenttext,{fontSize:13,color:'rgb(200,200,200)',lineHeight:15}]}>{'本队需要辅助1名,擅长XX英雄,战队福利优厚，报名从速'}</Text>
-            </View>
-            <TouchableOpacity style = {this.state.invite==0 ? styles.teambuttoninvite : styles.teambuttoninvited}  >
-                 <Text style = {this.state.invite==0 ? styles.btnfontinvite:styles.btnfontinvited}> { this.state.invite==0 ? '申请加入' : '已申请' } </Text>
-            </TouchableOpacity>
-           </View>
-            <View style={[styles.teamsplit]}></View>
-            <View style={[styles.teamlist]}>
-            <Image style={[styles.teamimage,]} source={{uri:'http://images.haigame7.com/logo/20160216133928XXKqu4W0Z5j3PxEIK0zW6uUR3LY=.png'}} />
-            <View style={[styles.teamcontent]}>
-            <Text style={[styles.teamcontenttext,{fontSize:15,color:'rgb(230, 193, 39)',fontWeight:'bold'}]}>{'犀利拍立冬至'}</Text>
-            <Text style={[styles.teamcontenttext,]}>{'生命不息,电竞不止'}</Text>
-            <Text style={[styles.teamcontenttext,{fontSize:13,color:'rgb(200,200,200)',lineHeight:15}]}>{'本队需要辅助1名,擅长XX英雄,战队福利优厚，报名从速'}</Text>
-            </View>
-            <TouchableOpacity style = {this.state.invite==0 ? styles.teambuttoninvite : styles.teambuttoninvited}  >
-                 <Text style = {this.state.invite==0 ? styles.btnfontinvite:styles.btnfontinvited}> { this.state.invite==0 ? '申请加入' : '已申请' } </Text>
-            </TouchableOpacity>
-            </View>
-            <View style={[styles.teamsplit]}></View>
-            <View style={[styles.teamlist]}>
-            <Image style={[styles.teamimage,]} source={{uri:'http://images.haigame7.com/logo/20160216133928XXKqu4W0Z5j3PxEIK0zW6uUR3LY=.png'}} />
-            <View style={[styles.teamcontent]}>
-            <Text style={[styles.teamcontenttext,{fontSize:15,color:'rgb(230, 193, 39)',fontWeight:'bold'}]}>{'犀利拍立冬至'}</Text>
-            <Text style={[styles.teamcontenttext,]}>{'生命不息,电竞不止'}</Text>
-            <Text style={[styles.teamcontenttext,{fontSize:13,color:'rgb(200,200,200)',lineHeight:15}]}>{'本队需要辅助1名,擅长XX英雄,战队福利优厚，报名从速'}</Text>
-            </View>
-            <TouchableOpacity style = {this.state.invite==1 ? styles.teambuttoninvite : styles.teambuttoninvited}  >
-                 <Text style = {this.state.invite==1 ? styles.btnfontinvite:styles.btnfontinvited}> { this.state.invite==1 ? '申请加入' : '已申请' } </Text>
-            </TouchableOpacity>
-            </View>
-             <View style={[styles.teamsplit]}></View>
+             {teamlist}
           </View>
      </View>
     );
   }
-});
+  _switchNavbar(nav){
+ var nameone ='我的申请';
+ var nametwo = '我的受邀';
+  if(nav==1){
+    nameone = '发出邀请';
+    nametwo = '申请加入';
+  }
+ this.setState({
+   navbar:nav,
+   data:{subnavbarone:nameone,subnavbartwo:nametwo},
+ });
+ return;
+  }
+}
 
 var styles = StyleSheet.create({
   container:{
@@ -270,4 +289,3 @@ var styles = StyleSheet.create({
  }
 
  });
-module.exports = Team;
