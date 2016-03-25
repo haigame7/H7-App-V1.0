362016987ec66d3181fc4871c87456c717657f46
Horace1117 <ghr1117@163.com>
2016-03-25 10:19:23 +0800

    2016年03月25日10:19:03

--- haigame7/app/constants/globalvariable.js
+++ haigame7/app/constants/globalvariable.js
@@ -3,6 +3,10 @@
 var userInfo = {
   'USERSESSION':'@haigame7:session',
 }
+var fightInfo={
+  'FightSend':'Send',
+  'FightReceive':'Receive',
+}
 var pageInfo = {
   'StartPage':1,
   'PageCount':5,
@@ -11,4 +15,5 @@ var pageInfo = {
 module.exports = {
   USER_INFO: userInfo,
   PAGE_INFO: pageInfo,
+  FIGHT_INFO: fightInfo,
 }
--- haigame7/ios/haigame7/AppDelegate.m
+++ haigame7/ios/haigame7/AppDelegate.m
@@ -33,7 +33,7 @@
 //
   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 //  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.10:8081/index.ios.bundle?platform=ios&dev=true"];
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.106:8081/index.ios.bundle?platform=ios&dev=true"];
+ // jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.106:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
