7c6c0ba5b183bf277bbdf67798742e5d93f7f9f0
Drex <382642143@qq.com>
2016-03-31 15:46:15 +0800

    UserGuess

--- haigame7/app/view/user/userguess.js
+++ haigame7/app/view/user/userguess.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * APPs我的赛事
+ * APP 我的竞猜
  * @return {[SplashScreen Component]}
  * @author aran.hu
  */
@@ -17,10 +17,12 @@ import React, {
     ScrollView,
     TouchableHighlight,
     } from 'react-native';
-var Util = require('../common/util');
-var Header = require('../common/headernav'); // 主屏
-var Icon = require('react-native-vector-icons/Iconfont');
-var commonstyle = require('../../styles/commonstyle');
+import Util from '../common/util';
+import Header from '../common/headernav'; // 主屏
+import Icon from 'react-native-vector-icons/Iconfont';
+import commonstyle from '../../styles/commonstyle';
+import styles from '../../styles/matchstyle';
+
 import Modal from 'react-native-modalbox';
 import Button from 'react-native-button';
 import GuessService from '../../network/guessservice';
@@ -90,40 +92,40 @@ export default class extends Component{
          }
    });
  }
- renderguessList(rowData){
+  renderguessList(rowData){
     return(
-      <View style={[styles.guesslist]}>
-       <View style={[styles.guessli]}>
-          <View style={[styles.guesslititle]}><Text style={[styles.guesslititletext]}>{rowData.MatchName}</Text></View>
-          <View style={[styles.guesslicontent]}>
-           <View style={[styles.flexcolumn]}>
-          <Image style={styles.guesslistimg} source={{uri:rowData.STeamLogo}} />
-          <Text style={[commonstyle.white, commonstyle.fontsize14,{marginTop:5}]}>{rowData.STeamName}</Text>
+      <View style={styles.guesslistblock}>
+        <View style={styles.guesslist}>
+          <View style={[styles.guesslisttitle]}>
+            <Text style={commonstyle.black}>{rowData.MatchName}</Text>
           </View>
-          <View style={styles.guesslistcenter}>
-            <Text style={[commonstyle.white, commonstyle.fontsize14]}>{'胜'}<Text>{'VS'}</Text>{'负'}</Text>
-            <Text style={[commonstyle.gray, commonstyle.fontsize12 ]}>{rowData.GuessTime}</Text>
+          <View style={[commonstyle.row, styles.guesslistcontent]}>
+            <View style={[commonstyle.col1, commonstyle.viewcenter]}>
+              <Image style={styles.guesslistimg} source={{uri:rowData.STeamLogo}} />
+              <Text style={[commonstyle.white, commonstyle.fontsize14, styles.guesslisttext]}>{rowData.STeamName}</Text>
+            </View>
+            <View style={[commonstyle.col1, commonstyle.viewcenter]}>
+              <Text style={[commonstyle.blue, commonstyle.fontsize22]}>{'VS'}</Text>
+              <Text style={[commonstyle.gray, commonstyle.fontsize12 ]}>{rowData.GuessTime}</Text>
+            </View>
+            <View style={[commonstyle.col1, commonstyle.viewcenter]}>
+              <Image style={styles.guesslistimg} source={{uri:'http://images.haigame7.com/logo/20160216133928XXKqu4W0Z5j3PxEIK0zW6uUR3LY=.png'}} />
+              <Text style={[commonstyle.white, commonstyle.fontsize14, styles.guesslisttext]}>{'犀利拍立冬至'}</Text>
+            </View>
           </View>
-          <View style={[styles.flexcolumn]}>
-         <Image style={styles.guesslistimg} source={{uri:'http://images.haigame7.com/logo/20160216133928XXKqu4W0Z5j3PxEIK0zW6uUR3LY=.png'}} />
-         <Text style={[commonstyle.white, commonstyle.fontsize14,{marginTop:5}]}>{'犀利拍立冬至'}</Text>
-         </View>
-         </View>
-         <View style={[styles.guessresult]}>
-         <View style={[styles.flexrow,{marginBottom:5}]}>
-           <Text style={[commonstyle.yellow, commonstyle.fontsize12]}>{'竞猜结果'}</Text>
-           <Text style={[commonstyle.white, commonstyle.fontsize12 ]}>{'  '}{rowData.Result}</Text>
-           <Text style={[commonstyle.yellow, commonstyle.fontsize12]}>{'       押注金额'}</Text>
-           <Text style={[commonstyle.white, commonstyle.fontsize12]}>{'  '}{rowData.BetMoney}{'氦金'}</Text>
-         </View>
-         <View style={[styles.flexrow,{marginBottom:5}]}>
-           <Text style={[commonstyle.yellow, commonstyle.fontsize12]}>{'我的选择'}</Text>
-           <Text style={[commonstyle.white, commonstyle.fontsize12 ]}>{'  '}{rowData.BetTeamName}</Text>
-           <Text style={[commonstyle.yellow, commonstyle.fontsize12]}>{'       竞猜收益'}</Text>
-           <Text style={[commonstyle.white, commonstyle.fontsize12]}>{'  '}{rowData.Result==GlobalVariable.GUESS_INFO.NoStart||GlobalVariable.GUESS_INFO.Starting?(rowData.BetMoney*rowData.Odds):0}{'氦金'}</Text>
-         </View>
-         </View>
-       </View>
+          <View style={styles.guesslistresult}>
+            <View style={[commonstyle.row, styles.guesslistresulttext]}>
+              <View style={commonstyle.col1}>
+                <Text style={[commonstyle.yellow, commonstyle.fontsize12]}>{'竞猜结果  '}<Text style={[commonstyle.white, commonstyle.fontsize12 ]}>{rowData.Result}</Text></Text>
+                <Text style={[commonstyle.yellow, commonstyle.fontsize12, styles.guesslistresulttextfont]}>{'我的选择  '}<Text style={[commonstyle.white, commonstyle.fontsize12 ]}>{rowData.BetTeamName}</Text></Text>
+              </View>
+              <View style={commonstyle.col1}>
+                <Text style={[commonstyle.yellow, commonstyle.fontsize12]}>{'押注金额  '}<Text style={[commonstyle.white, commonstyle.fontsize12]}>{rowData.BetMoney}{'氦金'}</Text></Text>
+                <Text style={[commonstyle.yellow, commonstyle.fontsize12, styles.guesslistresulttextfont]}>{'竞猜收益  '}<Text style={[commonstyle.white, commonstyle.fontsize12]}>{'  '}{rowData.Result==GlobalVariable.GUESS_INFO.NoStart||GlobalVariable.GUESS_INFO.Starting?(rowData.BetMoney*rowData.Odds):0}{'氦金'}</Text></Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -172,97 +174,18 @@ export default class extends Component{
       },1000);
     }
   }
-render() {
-  return (
-    <View style={styles.container}>
-     <Header screenTitle="我的竞猜"   navigator={this.props.navigator}></Header>
-
-          <View style={[styles.centerbg]}>
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header screenTitle="我的竞猜" navigator={this.props.navigator}/>
+        <ScrollView style={commonstyle.bodyer}>
           <ListView
             dataSource={this.state.dataguessSource}
             renderRow={this.renderguessList.bind(this)}
             renderFooter={this._renderFooter.bind(this)}
           />
-          </View>
-        </View>
+        </ScrollView>
+      </View>
     );
   }
 }
-var styles = StyleSheet.create({
-  container:{
-    flex:1,
-    marginTop:0
-  },
-  flexrow:{
-    flexDirection:'row',
-  },
-  flexcolumn:{
-    flexDirection:'column',
-    alignItems:'center',
-   justifyContent:'center',
-  },
-  centerbg: {
-     flex:1,
-     backgroundColor:'rgb(0, 0, 0)',
-     height: Util.size.height,
-     width: Util.size.width,
-  },
-  guesslist:{
-   flex:1,
-   paddingLeft:10,
-   paddingRight:10,
-
-  },
-  guessli:{
-  width: Util.size.width-20,
-  marginTop:10,
-  height:Util.size.height*13/40,
-  backgroundColor: '#232220',
-  borderColor:'gray',
-  borderWidth:1,
-
-  },
-  guesslititle:{
-   backgroundColor:'rgb(120,120,120)',
-   height:Util.pixel*50,
-   alignItems:'center',
-   justifyContent:'center',
-  },
-  guesslititletext:{
-   fontSize:14,
-  },
-  guesslicontent:{
-   paddingTop:20,
-   flexDirection:'row',
-   alignItems:'center',
-  justifyContent:'center',
-  },
-  guesslistimg: {
-     width: 70,
-     height: 70,
-     borderRadius: 35,
-     borderWidth: 2,
-     borderColor: 'rgba(255, 255, 255, 0.6)',
-     marginRight: 10,
-  },
-  guesslistcenter: {
-    width:Util.size.width/3,
-    alignItems:'center',
-   justifyContent:'center',
-  },
-  guesslisttext: {
-     marginTop: 5,
-     marginBottom: 5,
-  },
-  guessresult:{
-   backgroundColor:'rgba(182, 29, 38, 0.92)',
-     width:Util.size.width-60,
-     left:20,
-     height:60,
-     borderRadius:2,
-     top:10,
-   alignItems:'center',
-    justifyContent:'center',
-  },
-
- });
