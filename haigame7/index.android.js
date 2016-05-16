1c4d58910d465a11783a6985c619fa5c1ae4dce3
xiaoxuez <xiaoxue.zhang@haigame7.com>
2016-05-16 11:03:05 +0800

    Revert "4"
    This reverts commit 88cebb30b0e29c79ee44427162122842b4450f6d.

--- haigame7/index.android.js
+++ haigame7/index.android.js
@@ -2,5 +2,5 @@
 
 import React, {AppRegistry} from 'react-native';
 
-import App from './app/view/common/app.js';
+import App from './app/view/common/app';
 AppRegistry.registerComponent('haigame7', () => App);
